ebd716f385ec5b68a7c7afe930dead0ecc2cd7f1
E1230 <emaurobel1230@hotmail.com>
2023-07-03 19:15:39 -0500

    Mapeo de modelos

--- Models/Category.cs
+++ Models/Category.cs
@@ -1,8 +1,13 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace projectef.Models;
 
 public class Category
 {
+  [Key]
   public Guid CategoryId { get; set; }
+  [Required]
+  [MaxLength(150)]
   public string name { get; set; }
 
   public string description { get; set; }
